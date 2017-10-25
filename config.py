28a997d081f9ac4d93b587a51f5a10b091b6ec4d
hugo9099 <hugo9099@yahoo.com>
2017-10-25 00:16:49 -0400

    fixes

---
+++ config.py
@@ -0,0 +1,10 @@
+
+
+COINWARZ_PROFITS_URL = "https://www.coinwarz.com/cryptocurrency/?sha256hr=9460.00&sha256p=2600.00&sha256pc=0.1000&sha256c=false&scrypthr=110000.00&scryptp=1000.00&scryptpc=0.1000&scryptc=false&x11hr=34400.00&x11p=600.00&x11pc=0.1000&x11c=false&keccakhr=1000.00&keccakp=600.00&keccakpc=0.1000&keccakc=false&quarkhr=20000.00&quarkp=600.00&quarkpc=0.1000&quarkc=false&groestlhr=45.00&groestlp=825.00&groestlpc=0.1000&groestlc=false&blake256hr=11.20&blake256p=600.00&blake256pc=0.1000&blake256c=false&neoscrypthr=400.00&neoscryptp=400.00&neoscryptpc=0.1000&neoscryptc=false&lyra2rev2hr=30.00&lyra2rev2p=600.00&lyra2rev2pc=0.1000&lyra2rev2c=false&cryptonighthr=4140.00&cryptonightp=650.00&cryptonightpc=0.1200&cryptonightc=true&ethashhr=150.00&ethashp=900.00&ethashpc=0.1200&ethashc=true&equihashhr=1630&equihashp=835&equihashpc=0.1200&equihashc=true&e=Coinbase"
+
+# PANELS = ['', '', '']
+PANELS = ['panel1', 'panel2', 'panel3']
+
+
+ETH_WALLET = ""
+
